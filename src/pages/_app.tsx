4d7229d5a69d8eb81b359629d336aaf4748a409d
Davys Lima <davysjunior08@hotmail.com>
2021-04-07 00:34:57 -0300

    Feature: Home page, done.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app';
+import Header from '../components/Header';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +26,104 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.mainContainer}>
+        <header className={styles.headerContainer}>
+          <img src="/images/Logo.svg" alt="logo" />
+        </header>
+
+        <section className={styles.content}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.iconContainer}>
+            <div>
+              <FiCalendar color="#BBBBBB" />
+              <span>15 Mar 2021</span>
+            </div>
+            <div>
+              <FiUser color="#BBBBBB" />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.content}>
+          <h1>Criando um app CRA do zero</h1>
+          <p>
+            Tudo sobre como criar a sua primeira aplicação utilizando Create
+            React App.
+          </p>
+          <div className={styles.iconContainer}>
+            <div>
+              <FiCalendar color="#BBBBBB" />
+              <span>19 Abr 2021</span>
+            </div>
+            <div>
+              <FiUser color="#BBBBBB" />
+              <span>Danilo Vieira</span>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.content}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.iconContainer}>
+            <div>
+              <FiCalendar color="#BBBBBB" />
+              <span>15 Mar 2021</span>
+            </div>
+            <div>
+              <FiUser color="#BBBBBB" />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.content}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.iconContainer}>
+            <div>
+              <FiCalendar color="#BBBBBB" />
+              <span>15 Mar 2021</span>
+            </div>
+            <div>
+              <FiUser color="#BBBBBB" />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.content}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.iconContainer}>
+            <div>
+              <FiCalendar color="#BBBBBB" />
+              <span>15 Mar 2021</span>
+            </div>
+            <div>
+              <FiUser color="#BBBBBB" />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </section>
+
+        <span className={styles.loadMoreContent}>Carregar mais posts</span>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-
-//   // TODO
+// const prismic = getPrismicClient();
+// const postsResponse = await prismic.query(TODO);
+// TODO
 // };
